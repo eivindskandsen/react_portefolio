d994828617b8515cae157d552214e63871aa8581
Eivind Skandsen <eivind.skandsen@oxx.no>
2024-09-09 11:58:27 +0200

    update

---
+++ src/props/Parent.tsx
@@ -0,0 +1,6 @@
+import {Child} from './Child';
+import {ChildAsFC} from "./Child";
+
+const Parent = () => {
+    return <ChildAsFC color='red' onClick={()=> console.log("clicked")}>alskf</ChildAsFC>
+}
